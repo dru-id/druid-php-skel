c60b52188e4612200a10b3d28a9d827761dc2a6e
Germán Jiménez <german.jimenez@genetsis.com>
2016-02-04 16:44:40 +0100

    better integration of library

--- src/main/php/webapp/index.php
+++ src/main/php/webapp/index.php
@@ -1,3 +1,3 @@
 <html>
-project working fine
+    go to <a href="http://developers.dru-id.com/sdks/php-sdk/sdk-code-examples/">http://developers.dru-id.com/sdks/php-sdk/sdk-code-examples/</a> and play with examples
 </html>

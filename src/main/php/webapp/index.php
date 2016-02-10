62657a679891f3538b3b17713995085353fa0916
Alejandro Sánchez <alejandro.sanchez@genetsis.com>
2016-02-10 15:04:28 +0100

    improve welcome copies

--- src/main/php/webapp/index.php
+++ src/main/php/webapp/index.php
@@ -21,15 +21,15 @@ try {
 
 <body>
 <a href="http://developers.dru-id.com/" style="max-height: 88px;" target="_blank">
-    <img width="300" height="100" alt="DRUID Developers" src="/assets/img/Druid_logo_solo.png" style="max-height: 88px;" exify_intitialized="true">
+    <img width="300" height="100" alt="DRUID Developers" src="/assets/img/Druid_logo_solo.png" style="max-height: 88px;">
 </a>
 
 <h1>Welcome to DRUID</h1>
+<h2>Se how easy is integrate DRUID with your applications using the php SDK</h2>
 
-<h2>See how simple is integrate DRUID in your applications with our SDK</h2>
-<h3>(you have more examples in <a href="http://developers.dru-id.com/sdks/php-sdk/sdk-code-examples/">http://developers.dru-id.com/sdks/php-sdk/sdk-code-examples/</a>)</h3>
+<h3>(you have more examples available at <a href="http://developers.dru-id.com/sdks/php-sdk/sdk-code-examples/">http://developers.dru-id.com/sdks/php-sdk/sdk-code-examples/</a>)</h3>
 
-<h4>This page demostrates how to create login and register links for not connected user, and logout link and retrieve user email when user is connected:</h4>
+<h4>This page demostrates how to create login and registration links for not connected users, and show logout link and retrieve user email when user is connected:</h4>
 
 <p style="background-color:#e5ffff; border: thin solid #99ffff; padding: 20px">
 <?php
@@ -53,8 +53,6 @@ try {
 
 <h4>The code:</h4>
 <script src="https://gist.github.com/saspelo/3eba49c3be6e84cf4e9a.js"></script>
-
-<h4>Now you are able to develop your application integrated with DRUID.</h4>
 
 </body>
 
